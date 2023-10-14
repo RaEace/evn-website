65ea15c4d941867de043128ba442dc76dec10038
RaEace <julescool54@gmail.com>
2023-10-14 16:20:25 +0200

    feat: ini vite + react from vanilla vite pj

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-/// <reference types="vitest" />
-
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-react-ts-ghactions-template/',
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    sourcemap: true,
-  },
-  test: {
-    globals: true,
-    environment: 'happy-dom',
-    setupFiles: './src/setupTests.ts',
-    coverage: {
-      provider: 'c8',
-      reporter: ['html', 'lcov'],
-    },
-  },
-});
+  plugins: [react()],
+})
