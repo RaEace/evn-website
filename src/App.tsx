c4a986acf67fc7b121b3d2be290fc569e7cd1923
RaEace <julescool54@gmail.com>
2023-10-14 18:31:03 +0200

    feat: adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
 
 function App() {
-
   return (
     <>
-      <Home />
+    <BrowserRouter>
+       <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
